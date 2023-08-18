9724d6e011121c522e30638c90690d6ec981bb10
Matthew <Matthewtroberts10@gmail.com>
2023-08-17 20:16:48 -0500

    made changes to api to render on the screen

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,42 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  stocksData: any; // Declare a property to hold the API data
 
-const stocksContainer = document.querySelector('#firstStock');
-
-import axios from 'axios';
-
-const options = {
-  method: 'GET',
-  url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary',
-  params: {
-    symbol: 'AMRN',
-    region: 'US',
-  },
-  headers: {
-    'X-RapidAPI-Key': '362ec0421cmshfba1d04496e3fbbp12c938jsn16f3092359ee',
-    'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
-  },
-};
-
-const realTimeData = async function () {
-  try {
-    const response = await axios.request(options);
-    console.log(stocksContainer); //showing up as null?
-    console.log(response.data);
-    console.log(response.data.quoteType.symbol);
-    const html = `<div class="stocks_data">
-      <h3 class="quoteType.symbol">${response.data.quoteType.symbol}</h3>
-    </div>`;
-
-    // stocksContainer.insertAdjacentHTML('beforeend', html);
-  } catch (error) {
-    console.error(error);
+  ngOnInit(): void {
+    this.realTimeData(); 
   }
-};
-realTimeData();
+
+  realTimeData(): void {
+    const options = {
+      method: 'GET',
+      url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary',
+      params: {
+        symbol: 'AMRN',
+        region: 'US',
+      },
+      headers: {
+        'X-RapidAPI-Key': '362ec0421cmshfba1d04496e3fbbp12c938jsn16f3092359ee',
+        'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+      },
+    };
+
+    axios.request(options)
+      .then(response => {
+        this.stocksData = response.data; // Store the API data in the component property
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+}
